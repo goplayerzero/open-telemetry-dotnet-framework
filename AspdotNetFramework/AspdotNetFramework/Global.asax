8f37d1f881ccfb3a159407b094ea52010e670384
Kamlesh <kamlesh@playerzero.app>
2024-06-18 17:03:39 +0530

    added asp.net framework application

---
+++ AspdotNetFramework/AspdotNetFramework/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AspdotNetFramework.WebApiApplication" Language="C#" %>
---
+++ AspdotNetFramework/AspdotNetFramework/Global.asax.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using OpenTelemetry;
+using OpenTelemetry.Trace;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Logs;
+using Microsoft.Extensions.Logging;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Exporter;
+
+namespace AspdotNetFramework
+{
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        private TracerProvider _tracerProvider;
+        private MeterProvider _meterProvider;
+        private ILoggerFactory _loggerFactory;
+
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            var serviceName = "p0-dotnet-framework";
+            var endPoint = "https://sdk.playerzero.app/otlp";
+            var headers = "Authorization=Bearer 666af2fef6b93a24518cf726,x-pzprod=true";
+
+            _tracerProvider = Sdk.CreateTracerProviderBuilder()
+                .AddAspNetInstrumentation()
+                .AddConsoleExporter()
+                .AddSource(serviceName)
+                .SetResourceBuilder(
+                    ResourceBuilder.CreateDefault()
+                        .AddService(serviceName: serviceName, serviceVersion: "1.0.0"))
+                .AddOtlpExporter(options =>
+                {
+                    options.Endpoint = new Uri(endPoint + "/v1/traces");
+                    options.Headers = headers;
+                    options.Protocol = OtlpExportProtocol.HttpProtobuf;
+                })
+                .Build();
+
+            _meterProvider = Sdk.CreateMeterProviderBuilder()
+                .AddMeter(serviceName)
+                .AddConsoleExporter()
+                .AddOtlpExporter(options =>
+                {
+                    options.Endpoint = new Uri(endPoint + "/v1/metrics");
+                    options.Headers = headers;
+                    options.Protocol = OtlpExportProtocol.HttpProtobuf;
+                })
+                .Build();
+
+            _loggerFactory = LoggerFactory.Create(builder =>
+            {
+                builder.AddOpenTelemetry(logging =>
+                {
+                    logging.AddConsoleExporter();
+                    logging.AddOtlpExporter(options =>
+                    {
+                        options.Endpoint = new Uri(endPoint + "/v1/logs");
+                        options.Headers = headers;
+                        options.Protocol = OtlpExportProtocol.HttpProtobuf;
+                    });
+                });
+            });
+
+            var logger = _loggerFactory.CreateLogger<WebApiApplication>();
+            logger.LogInformation("Application started and OpenTelemetry configured.");
+
+            Application["LoggerFactory"] = _loggerFactory;
+        }
+
+        protected void Application_End()
+        {
+            _tracerProvider?.Dispose();
+            _meterProvider?.Dispose();
+            _loggerFactory?.Dispose();
+        }
+    }
+}
