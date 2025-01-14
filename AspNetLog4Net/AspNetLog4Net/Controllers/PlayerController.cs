eeb519a819a43eadf6ea12d9d6e8883593fa0ea8
kamleshplayerzero <106535429+kamleshplayerzero@users.noreply.github.com>
2025-01-14 18:59:35 +0530

    Log4Net and open telemetry (#20)

---
+++ AspNetLog4Net/AspNetLog4Net/Controllers/PlayerController.cs
@@ -0,0 +1,180 @@
+﻿using AspNetLog4Net.Models;
+using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using log4net;
+
+
+namespace AspNetLog4Net.Controllers
+{
+    public class PlayerController : Controller
+    {
+        private string connectionString = ConfigurationManager.ConnectionStrings["P0SQLConnection"].ConnectionString;
+
+        private static readonly ILog logger = LogManager.GetLogger(typeof(PlayerController));
+
+        public List<PlayerModel> GetPlayerListFromDb()
+        {
+            List<PlayerModel> players = new List<PlayerModel>();
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                using (SqlCommand command = new SqlCommand("GetAllPlayers", connection))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+
+                    connection.Open();
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            PlayerModel player = new PlayerModel
+                            {
+                                Id = (int)reader["id"],
+                                Name = reader["name"].ToString(),
+                                Description = reader["description"].ToString()
+                            };
+                            players.Add(player);
+                        }
+                    }
+                }
+            }
+
+            return players;
+        }
+
+        public PlayerModel GetPlayerDetailsBySql(int id)
+        {
+            PlayerModel player = null;
+            try
+            {
+                if (id <= 0)
+                {
+                    logger.Error($"ID must be a positive integer. {id}");
+                    throw new ArgumentException("ID must be a positive integer.", nameof(id));
+                }
+                else if (id > 10)
+                {
+                    logger.Error($"GetPlayerDetailsBySql - You are requesting for {id}, it should be less than or equal to 10");
+                }
+
+                string query = "SELECT Id, Name, Description FROM Players WHERE Id = @PlayerId";
+
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        command.CommandType = CommandType.Text;
+                        command.Parameters.AddWithValue("@PlayerId", id);
+
+                        connection.Open();
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            if (reader.Read())
+                            {
+                                player = new PlayerModel
+                                {
+                                    Id = (int)reader["Id"],
+                                    Name = reader["Name"].ToString(),
+                                    Description = reader["Description"].ToString()
+                                };
+                            }
+                        }
+                    }
+                }
+            }
+            catch (ArgumentException ex)
+            {
+                logger.Error($"ArgumentException in GetPlayerDetailsBySql: {ex}");
+                throw new Exception($"ArgumentException in GetPlayerDetailsBySql: {ex}");
+            }
+            catch (SqlException ex)
+            {
+                logger.Error($"Database error in GetPlayerDetailsBySql: {ex}");
+                throw new Exception($"Database error in GetPlayerDetailsBySql: {ex}");
+            }
+            catch (Exception ex)
+            {
+                logger.Error($"GetPlayerDetailsBySql: {ex}");
+                throw new Exception($"GetPlayerDetailsBySql: {ex}");
+            }
+
+            return player;
+        }
+
+        public PlayerModel GetPlayerDetailsFromDb(int id)
+        {
+            if (id <= 0)
+            {
+                logger.Error($"GetPlayerDetailsFromDb ID must be a positive integer. {id}");
+                throw new ArgumentException("ID must be a positive integer.", nameof(id));
+            }   
+
+            PlayerModel player = null;
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                var storeProcName = id > 10 ? "NotFoundQuery" : "GetPlayerDetails";
+                
+                using (SqlCommand command = new SqlCommand(storeProcName, connection))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("@PlayerId", id);
+
+                    connection.Open();
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            player = new PlayerModel
+                            {
+                                Id = (int)reader["id"],
+                                Name = reader["name"].ToString(),
+                                Description = reader["description"].ToString()
+                            };
+                        }
+                    }
+                }
+            }
+
+            return player;
+        }
+
+        // GET: Player
+        public ActionResult Index()
+        {
+            var list = GetPlayerListFromDb();
+            ViewBag.PlayerList = list;
+            ViewBag.PlayerDetails = new PlayerModel();
+
+            var playerId = 23;
+            var userService = new UserService();
+
+            var user = userService.GetUser(playerId);
+
+            var playerDetails = GetPlayerDetailsBySql(playerId);
+            ViewBag.PlayerDetails = playerDetails;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult GetPlayerDetails(int playerId)
+        {
+            var list = GetPlayerListFromDb();
+            ViewBag.PlayerList = list;
+            logger.Warn($"Current Trace ID {Activity.Current.TraceId}");
+
+            var userService = new UserService();
+
+            var user = userService.GetUser(playerId);
+
+            var playerDetails = GetPlayerDetailsBySql(playerId);
+            ViewBag.PlayerDetails = playerDetails;
+            return View("Index");
+        }
+    }
+}
---
+++ AspNetLog4Net/AspNetLog4Net/Global.asax.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using OpenTelemetry;
+using OpenTelemetry.Trace;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Logs;
+using Microsoft.Extensions.Logging;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Exporter;
+using System.Diagnostics;
+using log4net;
+
+namespace AspNetLog4Net
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        private TracerProvider _tracerProvider;
+        private MeterProvider _meterProvider;
+        private ILoggerFactory _loggerFactory;
+
+        private static readonly ActivitySource activitySource = new ActivitySource("My Dataset Name"); //Keep it same as your service name
+
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            
+            var serviceName = "My Dataset Name";
+            var serviceVersion = "1.0.0";
+
+            var endPoint = "https://sdk.playerzero.app/otlp";
+            var headers = "Authorization=Bearer <api_token>" +
+                ",x-pzprod=true";
+
+            var resourceBuilder = ResourceBuilder.CreateDefault()
+               .AddService(serviceName: serviceName, serviceVersion: serviceVersion);
+
+            _tracerProvider = Sdk.CreateTracerProviderBuilder()
+                .SetResourceBuilder(resourceBuilder)
+                .AddAspNetInstrumentation()
+                .AddHttpClientInstrumentation()
+                .AddSqlClientInstrumentation(options =>
+                {
+                    options.SetDbStatement = true;
+                })
+                .AddSource(serviceName)
+                .AddOtlpExporter(options =>
+                {
+                    options.Endpoint = new Uri(endPoint + "/v1/traces");
+                    options.Headers = headers;
+                    options.Protocol = OtlpExportProtocol.HttpProtobuf;
+                })
+                .Build();
+
+            _meterProvider = Sdk.CreateMeterProviderBuilder()
+                .SetResourceBuilder(resourceBuilder)
+                .AddMeter(serviceName)
+                .AddOtlpExporter(options =>
+                {
+                    options.Endpoint = new Uri(endPoint + "/v1/metrics");
+                    options.Headers = headers;
+                    options.Protocol = OtlpExportProtocol.HttpProtobuf;
+                })
+                .Build();
+            
+            _loggerFactory = LoggerFactory.Create(builder =>
+            {
+                builder.AddOpenTelemetry(logging =>
+                {
+                    logging.SetResourceBuilder(resourceBuilder);
+                    logging.AddOtlpExporter(options =>
+                    {
+                        options.Endpoint = new Uri(endPoint + "/v1/logs");
+                        options.Headers = headers;
+                        options.Protocol = OtlpExportProtocol.HttpProtobuf;
+                    });
+                });
+            });            
+
+            log4net.Config.XmlConfigurator.Configure();
+
+            LogManager.GetRepository().Properties["ILoggerFactory"] = _loggerFactory;
+
+        }
+
+        protected void Application_End()
+        {
+            _tracerProvider?.Dispose();
+            _meterProvider?.Dispose();
+            _loggerFactory?.Dispose();
+        }
+
+        protected void Application_BeginRequest()
+        {
+            HttpCookie traceIdCookie = HttpContext.Current.Request.Cookies["pz-traceid"];
+            if (traceIdCookie != null)
+            {
+                string traceId = traceIdCookie.Value;
+
+                // Convert the string traceId to ActivityTraceId
+                ActivityTraceId pzTraceId = ActivityTraceId.CreateFromString(traceId.AsSpan());
+
+                var spanId = ActivitySpanId.CreateRandom();
+
+                var activityContext = new ActivityContext(pzTraceId, spanId, ActivityTraceFlags.Recorded);
+
+                Activity.Current.Stop();
+
+                // Create and start the activity
+                var activity = activitySource.StartActivity("CustomActivity", ActivityKind.Server, activityContext);
+
+                if (activity != null)
+                {
+                    Activity.Current = activity;
+                    
+                    log4net.ThreadContext.Properties["TraceId"] = activity.TraceId.ToString();
+                    log4net.ThreadContext.Properties["SpanId"] = activity.SpanId.ToString();
+                }
+                
+                Debug.WriteLine($"Activity started with TraceId: {Activity.Current?.TraceId}");
+            }
+        }
+    }
+}
